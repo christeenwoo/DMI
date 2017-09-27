03b3833cce2b26eecc39eca413a4c385f6233acf
lionika88@mail.ru <voicissedgars@gmail.com>
2017-09-27 11:48:16 +0300

    20170927_11_50

--- shell_programming.sh
+++ shell_programming.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 NAME="Vaards Uzvaards"
-#echo NAME
+#4.piemers
+echo "Tiek pildits skripts: "$0
+#echo $0
+#echo $n
+echo "Skriptam nodoto argumentu skaits: " $#
+echo"Skriptam nodoti argumenti (attelosona/grupesana 1)": $*
+echo "Skriptam nodoti argumenti (attelosana/grupesana 2)": $@
+#echo $?
+echo "Skriptam pieskirtais procesa numurs" $$
+#echo $!
 
-
-#readonly NAME
-#echo $NAME
-
-#NAME="Uzvaards Vaards"
-#echo $NAME
 
 echo $NAME
 unset NAME
 echo $NAME
+#3.piemers
+#NAME= "Vaards Uzvaards"
+#echo $NAME
+#echo $unset
+#echo $NAME
+
+#2.piemers
+#readonly NAME
+#echo $NAME
+#NAME="Uzvaards Vaards"
+#echo $NAME
+
+#1. piemers
+#echo NAME
 
 
